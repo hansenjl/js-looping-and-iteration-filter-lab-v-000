5fb84c0d0a0d64ee40d69275cd04245658f46e7a
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-12 23:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(array,name){
-  array.filter(string => string === name)
+  return array.filter(string => string === name)
 }
 
 function fuzzyMatch(){

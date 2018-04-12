591f03c83c52b90ac1d3067c85b16b25fa10d389
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-12 23:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(array,name){
-  return array.filter(string => string === name)
+  return array.filter(string => string.toLowerCase() === name.toLowerCase())
 }
 
 function fuzzyMatch(){
